0101e49100f794a3080653f0c9afa29c920b2966
Mahdi HARZALLAH <mahdihrz@gmail.com>
2022-03-08 10:03:00 +0100

    init generated by spring

---
+++ src/main/java/com/mahdihrz/javaspringwssample/JavaSpringWsSampleApplication.java
@@ -0,0 +1,13 @@
+package com.mahdihrz.javaspringwssample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringWsSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringWsSampleApplication.class, args);
+	}
+
+}
